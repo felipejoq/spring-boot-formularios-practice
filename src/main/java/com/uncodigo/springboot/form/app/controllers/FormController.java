8cf833bb7f4686e3c2de43e3cf467fe7406147f0
felipejoq <felipejofreq@gmail.com>
2022-12-10 11:48:14 -0300

    Recibiendo los datos del form.html con la anotación 'RequestParam' en el controlador, forma básica y para casos específicos

--- src/main/java/com/uncodigo/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/uncodigo/springboot/form/app/controllers/FormController.java
@@ -4,19 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		
+		model.addAttribute("titulo", "Formulario de prueba para usuarios:");
 
 		return "form";
 
 	}
 
 	@PostMapping("/form")
-	public String procesarForm(Model model) {
+	public String procesarForm(Model model,
+			@RequestParam("username") String username, // Se le puede indicar explícitamente el nombre del parámetro.
+			@RequestParam String password,
+			@RequestParam String email) {
+		
+		model.addAttribute("titulo", "Resultado del from:");
+		
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		model.addAttribute("email", email);
 
 		return "resultado";
 	}
