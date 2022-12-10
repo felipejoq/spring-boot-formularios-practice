a9ee136e85ce0fdab700cf290a8d50fc984cfc93
felipejoq <felipejofreq@gmail.com>
2022-12-10 13:26:16 -0300

    Usando helpers de thymeleaf para mostrar los errores y manejar los atributos del objeto en el formulario para tener un código más limpio en la vista y en el controlador

--- src/main/java/com/uncodigo/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/uncodigo/springboot/form/app/controllers/FormController.java
@@ -1,13 +1,13 @@
 package com.uncodigo.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,21 +31,22 @@ public class FormController {
 	}
 
 	@PostMapping("/form")
-	public String procesarForm(@Valid Usuario usuario, BindingResult result, Model model
-	/*
-	 * @ModelAttribute("user") -> Para definir un nombre de variable personalizado y no el nombre
-	 * de la clase que crea el objeto.
-	 * 
-	 * Primero va el objeto a recibir y valida, Luego BindingResult para recibir los
-	 * resultados de la validación. BindingResult
-	 * 
-	 * @RequestParam("username") String username, -> Se le puede indicar
-	 * explícitamente el nombre del parámetro.
-	 * 
-	 * @RequestParam String password
-	 * 
-	 * @RequestParam String email
-	 */) {
+	public String procesarForm(@Valid Usuario usuario, BindingResult result, Model model) {
+		
+		/*
+		 * @ModelAttribute("user") -> Para definir un nombre de variable personalizado y no el nombre
+		 * de la clase que crea el objeto.
+		 * 
+		 * Primero va el objeto a recibir y valida, Luego BindingResult para recibir los
+		 * resultados de la validación. BindingResult
+		 * 
+		 * @RequestParam("username") String username, -> Se le puede indicar
+		 * explícitamente el nombre del parámetro.
+		 * 
+		 * @RequestParam String password
+		 * 
+		 * @RequestParam String email
+		 */
 
 		model.addAttribute("titulo", "Resultado del from:");
 
@@ -54,12 +55,17 @@ public class FormController {
 			 * Usando map para crear una lista de clave valor y así enviar los mensajes de
 			 * error a la vista.
 			 */
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(error -> {
-				errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
-			});
+			/*
+			 * Para pasar de forma más "básica" la lista de errores a la vista.
+			 * 
+			 * Map<String, String> errores = new HashMap<>();
+			 * result.getFieldErrors().forEach(error -> { errores.put(error.getField(),
+			 * "El campo ".concat(error.getField()).concat(" ").concat(error.
+			 * getDefaultMessage())); });
+			 * model.addAttribute("error", errores);
+			 */
 			
-			model.addAttribute("error", errores);
+			
 
 			return "form";
 		}
